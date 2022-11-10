9d2a3fbd89d6bed7a7b2014c1cf23baa6fb219f6
pillypenkosa <pillypenkosa@gmail.com>
2022-11-10 19:38:32 +0200

    2022-11-10 19:35

--- component/surnames/cmp.surnames.js
+++ component/surnames/cmp.surnames.js
@@ -11,8 +11,9 @@ class ComponentSurnames {
 	static html() {
 
 		let innerHTML = '';
-		arrSurnames.forEach( k => {
+		arrSurnames.forEach( ( k, i ) => {
 
+			//<span class="val">${ i + 1 })</span> 
 			innerHTML += ComponentKeyval.html( `
 				<span class="key">id:</span> <span class="val">${ k.id }</span> 
 				<span class="key">title:</span> <span class="val">${ k.title }</span>
