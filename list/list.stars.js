664a0756b2f4eefe60a1e356e7a622e45bc8cd13
pillypenkosa <pillypenkosa@gmail.com>
2023-01-08 13:55:04 +0200

    DJS

--- list/list.stars.js
+++ list/list.stars.js
@@ -10840,6 +10840,7 @@ https://uk.wikipedia.org/wiki/20_%D0%B1%D0%B5%D1%80%D0%B5%D0%B7%D0%BD%D1%8F#%D0%
 	id: 'yolka_02071982', 
 	name: { n: 'Ёлка', },
 	life: { bd: 2, bm: 7, by: 1982, },
+	img: [ 1, 1 ],
 	country: { ukr: 1, our: { sng: 1, }, },
 	music: { singer: {}, },
 	internet: {
